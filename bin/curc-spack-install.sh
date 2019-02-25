989451b77f77c143eb8dd6c0b98a9ff5b3680724
Jonathon Anderson <jonathon.anderson@colorado.edu>
2019-02-25 11:17:27 -0700

    Install openmpi with intel

--- bin/curc-spack-install.sh
+++ bin/curc-spack-install.sh
@@ -7,44 +7,61 @@ set -v
 # See packages.yaml for additional specifications
 
 
-spack clean
+function main
+{
+    spack clean
 
 
-#
-# core
+    #
+    # core
 
-CORE_COMPILER='gcc@4.8.5'
-CORE_PACKAGES=(
-    lmod
-    gcc@8.2.0
-    #intel@18.0.3
-    #intel-parallel-studio@cluster.2018.3
-    #pgi@18.4
-)
-for package in "${CORE_PACKAGES[@]}"
-do
-    spack install "${package}" "%${CORE_COMPILER}"
-done
+    CORE_COMPILER='gcc@4.8.5'
+    CORE_PACKAGES=(
+        lmod
+        gcc@8.2.0
+        intel@18.0.3
+        intel-parallel-studio@cluster.2018.3
+        #pgi@18.4
+    )
+    for package in "${CORE_PACKAGES[@]}"
+    do
+        spack install "${package}" "%${CORE_COMPILER}"
+    done
 
-spack install openmpi %gcc@8.2.0
-spack install hdf5 %gcc@8.2.0
-spack install netcdf %gcc@8.2.0
-spack install parallel-netcdf %gcc@8.2.0
+    spack install openmpi %gcc@8.2.0
+    spack install openmpi %intel@18.0.3 \
+          ^/$(gethash hwloc%gcc@8.2.0) \
+          ^/$(gethash ucx%gcc@8.2.0) \
+          ^/$(gethash slurm%gcc@8.2.0) \
+          ^/$(gethash libfabric%gcc@8.2.0)
 
-#spack install openmpi %pgi@18.4 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}"
-#spack install openmpi %intel@18.0.3 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}"
+    spack install hdf5 %gcc@8.2.0
+    spack install netcdf %gcc@8.2.0
+    spack install parallel-netcdf %gcc@8.2.0
 
-# spack install parallel-netcdf %intel@18.0.3 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}" ^m4%gcc@8.2.0
-# spack install parallel-netcdf %pgi@18.4 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}" ^m4%gcc@8.2.0
+    #spack install openmpi %pgi@18.4 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}"
 
-# spack install hdf5 %intel@18.0.3 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}"
-# spack install hdf5 %pgi@18.4 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}"
+    # spack install parallel-netcdf %intel@18.0.3 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}" ^m4%gcc@8.2.0
+    # spack install parallel-netcdf %pgi@18.4 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}" ^m4%gcc@8.2.0
 
-# spack install netcdf %intel@18.0.3 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}" ^m4%gcc@8.2.0
-# spack install netcdf %pgi@18.4 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}" ^m4%gcc@8.2.0
+    # spack install hdf5 %intel@18.0.3 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}"
+    # spack install hdf5 %pgi@18.4 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}"
+
+    # spack install netcdf %intel@18.0.3 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}" ^m4%gcc@8.2.0
+    # spack install netcdf %pgi@18.4 "${LOCAL_OPENMPI_DEPS[@]}" "${GCC_OPENMPI_DEPS[@]}" ^m4%gcc@8.2.0
 
 
-#
-# lmod sync
+    #
+    # lmod sync
 
-spack module lmod refresh -y
+    spack module lmod refresh -y
+}
+
+
+function gethash
+{
+    basename $(spack location -i $1) | grep -o '[^-]*$' | head -n 1
+}
+
+
+main "$@"
