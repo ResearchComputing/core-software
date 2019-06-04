9db1dac4a3b6496da9b6799142e6e73c5b52708d
Jonathon Anderson <jonathon.anderson@colorado.edu>
2019-06-03 23:38:24 -0600

    Add r, rstudio install to anaconda install

--- bin/curc-anaconda-install.sh
+++ bin/curc-anaconda-install.sh
@@ -5,6 +5,8 @@ bash /curc/sw/anaconda2/Anaconda2-2019.03-Linux-x86_64.sh -b -p /curc/sw/anacond
     eval "$(/curc/sw/anaconda2/2019.03/bin/conda shell.bash hook)"
     conda config --add channels intel
     conda create -n idp intelpython2_full python=2
+
+    conda install r rstudio
 )
 
 bash /curc/sw/anaconda3/Anaconda3-2019.03-Linux-x86_64.sh -b -p /curc/sw/anaconda3/2019.03
@@ -12,4 +14,6 @@ bash /curc/sw/anaconda3/Anaconda3-2019.03-Linux-x86_64.sh -b -p /curc/sw/anacond
     eval "$(/curc/sw/anaconda3/2019.03/bin/conda shell.bash hook)"
     conda config --add channels intel
     conda create -n idp intelpython3_full python=3
+
+    conda install r rstudio
 )
